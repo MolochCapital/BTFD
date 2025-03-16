dddcb1dc131b8e535956a98fd7cab3b22dfde827
rootdraws <rootdraws@gmail.com>
2025-03-16 18:54:44 -0500

    Setup Test Environment.

---
+++ hardhat.config.ts
@@ -0,0 +1,43 @@
+import {HardhatUserConfig} from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.19', // solidity version
+  defaultNetwork: 'mantleSepolia', // chosen by default when network isn't specified while running Hardhat
+  networks: {
+    mantle: {
+      url: 'https://rpc.mantle.xyz', //mainnet
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY ?? ''],
+    },
+    mantleSepolia: {
+      url: 'https://rpc.sepolia.mantle.xyz', // Sepolia Testnet
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY ?? ''],
+      gasPrice: 20000000,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+    customChains: [
+      {
+        network: 'mantle',
+        chainId: 5000,
+        urls: {
+          apiURL: 'https://api.mantlescan.xyz/api',
+          browserURL: 'https://mantlescan.xyz',
+        },
+      },
+      {
+        network: 'mantleSepolia',
+        chainId: 5003,
+        urls: {
+          apiURL: 'https://api-sepolia.mantlescan.xyz/api',
+          browserURL: 'https://sepolia.mantlescan.xyz/',
+        },
+      },
+    ],
+  },
+};
+export default config;
